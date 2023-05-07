e67d2bf5ce8c520488f612b6963deaf6a0b2382d
Ygilvan <gilvansc1234@gmail.com>
2023-05-07 03:55:17 -0300

    first commit

--- src/firstDuplicate/main.java
+++ src/firstDuplicate/main.java
@@ -1,7 +1,17 @@
 package firstDuplicate;
 
-public class main {
-    public static void main(String[] args) {
+import java.lang.reflect.Array;
 
+public class main {
+    private static int[] a = {2,3,2,4,1};
+    public static void main(String[] args) {
+        System.out.println(getFirstDuplicate(a));
+    }
+    private static int getFirstDuplicate(int[] a){
+        for (int i = 0; i < a.length; i++) {
+            int firstNumber= Array.getInt(i);
+            return i;
+        }
+        return -1;
     }
 }
